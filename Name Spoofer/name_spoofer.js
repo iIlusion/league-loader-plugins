1a08fa5eed0ce4df58d54b8a4a2be38bb1dda607
sickrat <breevzeditor@gmail.com>
2023-02-21 11:13:52 -0300

    remove console.log

--- Name Spoofer/name_spoofer.js
+++ Name Spoofer/name_spoofer.js
@@ -11,7 +11,6 @@ window.addEventListener("load", () => {
                 let elements = document.querySelectorAll(
                     ".style-profile-summoner-name, .name, .match-details-team-list .player-history-mode, .custom-member-name, .name-text, .player-name, .parties-player-name, .rank-standing-row-component.lol-leagues-list-item.me.current.ember-view .lol-leagues-list-col-3.lol-leagues-list-col, .prompted-voting-candidate-name, .scoreboard-row-player-name"
                 );
-                console.log(elements);
                 if (elements.length >= 1) {
                     for (var i = 0; i < elements.length; i++) {
                         elements[i].textContent = name;
