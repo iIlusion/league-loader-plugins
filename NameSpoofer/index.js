193588362cb8001d639440fb990d007d150ff537
Soulmare <127675679+Yurichtofen@users.noreply.github.com>
2023-05-21 02:11:36 +0200

    Name Spoofer
    Improvements?

--- NameSpoofer/index.js
+++ NameSpoofer/index.js
@@ -1,26 +1,25 @@
 import data from "./config.json";
-let enabled = data.spoof;
-let name = data.name;
+
+const { spoof, name } = data;
 
 window.addEventListener("load", () => {
-    const interval = setInterval(() => {
-        if (!enabled) return clearInterval(interval);
-        const manager = document.getElementById("rcp-fe-viewport-root");
-        if (manager) {
-            clearInterval(interval);
-            new MutationObserver((mutations) => {
-                let elements = document.querySelectorAll(
-                    ".style-profile-summoner-name, .name, .match-details-team-list .player-history-mode, .custom-member-name, .name-text, .player-name, .parties-player-name, .rank-standing-row-component.lol-leagues-list-item.me.current.ember-view .lol-leagues-list-col-3.lol-leagues-list-col, .prompted-voting-candidate-name, .scoreboard-row-player-name"
-                );
-                if (elements.length >= 1) {
-                    for (var i = 0; i < elements.length; i++) {
-                        elements[i].textContent = name;
-                    }
-                }
-            }).observe(manager, {
-                childList: true,
-                subtree: true,
-            });
-        }
-    }, 500);
+  const interval = setInterval(() => {
+    if (!spoof) return clearInterval(interval);
+    const manager = document.getElementById("rcp-fe-viewport-root");
+    if (manager) {
+      clearInterval(interval);
+      new MutationObserver((mutations) => {
+        const elements = document.querySelectorAll(
+          ".banner-summoner-name, .chat-gnt, .chat-name, .conversation-title, .custom-member-name, .hover-card-game-name, .hover-card-game-tag, .hover-card-name, .lol-leagues-list-col-3.lol-leagues-list-col, .lol-leagues-list-item.me.current.ember-view .rank-standing-row-component, .match-details-team-list .player-history-mode, .member-name, .name, .name-text, .parties-player-name, .player-name, .prompted-voting-candidate-name, .scoreboard-row-player-name, .style-profile-search-trail-summoner-name, .style-profile-summoner-name"
+        );
+
+        elements.forEach((element) => {
+          element.textContent = name;
+        });
+      }).observe(manager, {
+        childList: true,
+        subtree: true,
+      });
+    }
+  }, 500);
 });
