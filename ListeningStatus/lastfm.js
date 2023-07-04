8f027d753c2548d8e0b3601b62e669fb1191d269
unknown <breevzeditor@gmail.com>
2023-07-04 06:07:32 -0300

    push fixs

--- ListeningStatus/lastfm.js
+++ ListeningStatus/lastfm.js
@@ -1,15 +1,15 @@
 import axios from "https://esm.run/axios";
-import config from "./config.json";
+import config from "./config.json"
 
 const lastfmUrl = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${config.LASTFM.user}&api_key=${config.LASTFM.key}&format=json&limit=1`;
 
 export async function nowPlayingFM() {
-    const response = await axios.get(lastfmUrl);
+    const response = await axios.get(lastfmUrl)
     const track = response.data.recenttracks.track[0];
     if (track["@attr"] && track["@attr"].nowplaying) {
         return {
             name: track.name,
-            artist: track.artist["#text"],
-        };
+            artist: track.artist["#text"]
+        }
     } else return false;
 }
